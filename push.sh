fdd8a423f3f3a4a2a040a788e3135dc2492b327a
coldpaper1 <jongchan4869@gmail.com>
2022-05-12 02:04:07 +0900

    hi

--- push.sh
+++ push.sh
@@ -2,30 +2,51 @@
 
 company=""
 message=""
+loop="loop"
 
 #echo -e "\t\tcompany select"
 #echo -en "\t     1.aws 2.gcp 3.azure"
 echo -e "\t     1.aws 2.gcp 3.azure"
 echo "=============================================="
-echo -e "\t\tcompany select : "
+
+
+
+while [[ -n "$loop" ]]
+do
+
+echo -en "\t\tcompany select : "
 read company
 echo
-
+	
 if [ $company -eq 1 ]
 then
-
+	company="aws"
+	loop=""
 elif [ $company -eq 2 ]
-
+then
+	company="gcp"
+	loop=""
 
 
 elif [ $company -eq 3 ]
+then
+	company="azure"
+	loop=""
+else
+	echo -e "\t\tInvalid input."
+fi
+done
 
 
+echo -e "\t\t     $company"
 
+echo $company > name.txt
+echo
+git add .
+echo -en "\t\tEnter your message : "
+read message
 
-
-echo -e "\t\t     $companyname"
-
-
-
-
+git commit -m "${message}"
+git push origin master
+echo
+echo -e "\t\tgit push susuccess!!"
